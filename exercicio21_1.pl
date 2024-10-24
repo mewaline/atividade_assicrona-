b01aad7eb37a94a7eb32bb6f5422560de270fac4
ALINE SILVA DA CUNHA <148893023+mewaline@users.noreply.github.com>
2024-10-24 17:42:18 -0400

    Update exercicio21_1.pl

--- exercicio21_1.pl
+++ exercicio21_1.pl
@@ -1,15 +1,12 @@
-max_proof_length(10).
-max_clause_length(3).
-
 prove(Goal, Hypo, Answer):-
     max_proof_length(D),
     prove(Goal, Hypo, D, RestD),
     (RestD >= 0, Answer = yes
-    ;				     
+     ;                     
      RestD < 0, Answer = maybe).
-prove(Goal, _, no).
+prove(_, _, no).
 
-prove(G, H, D, D):-
+prove(_, _, D, D):-
     D < 0, !.
 prove([], _, D, D):- !.
 prove([G1|Gs],Hypo,D0,D):-
@@ -20,13 +17,12 @@ prove(G,_,D,D):-
     call(G).
 prove(G,Hypo,D0,D):-
     D0 =< 0, !,
-    D is D0-1
-    ;
+    D is D0-1;
     D1 is D0 - 1,
-    member(Clause/Vars, Hypo),
-    copy_term(Clause,[Head|Body]),
+    member(Clause/_ , Hypo),
+    copy_term(Clause, [Head|Body]),
     G = Head,
-    prove(Body, Hypo,D1,D).
+    prove(Body, Hypo, D1, D).
 
 induce(Hyp):-
     iter_deep(Hyp,0).
@@ -35,8 +31,7 @@ iter_deep(Hyp,MaxD):-
     write('MaxD= '), write(MaxD), nl,
     start_hyp(Hyp0),
     complete(Hyp0),
-    depth_first(Hyp0,Hyp,MaxD)
-    ;
+    depth_first(Hyp0,Hyp,MaxD);
     NewMaxD is MaxD+1,
     iter_deep(Hyp, NewMaxD).
 
@@ -50,23 +45,22 @@ depth_first(Hyp0,Hyp,MaxD0):-
     depth_first(Hyp1,Hyp,MaxD1).
 
 complete(Hyp):-
-    not(ex(E),
+    not((ex(E),
         once(prove(E, Hyp, Answer)),
-        Answer \== yes).
+        Answer \== yes)).
 
 consistent(Hyp):-
-    not(nex(E),
+    not((nex(E),
         once(prove(E, Hyp, Answer)),
-        Answer \== no).
+        Answer \== no)).
 
 refine_hyp(Hyp0,Hyp):-
-    conc(Clauses1,[Clause0/Vars0 | Clauses2], Hyp0),
-    conc(Clauses1,[Clause/Vars | Clauses2], Hyp),
-    refine(Clause0, Vars0, Clause, Vars).
+    conc(Clauses1,[Clause0/_ | Clauses2], Hyp0),
+    conc(Clauses1,[Clause/_ | Clauses2], Hyp),
+    refine(Clause0, _, Clause, _).
 
 refine(Clause, Args, Clause, NewArgs):-
-    conc(Args1, [A | Args2], Args),
-    member(A, Args2),
+    conc(Args1, [_ | Args2], Args),
     conc(Args1, Args2, NewArgs).
 refine(Clause,Args,NewClause, NewArgs):-
     length(Clause, L),
@@ -76,15 +70,12 @@ refine(Clause,Args,NewClause, NewArgs):-
     conc(Clause,[Lit],NewClause),
     conc(Args, Vars, NewArgs).
 
+max_proof_length(10).
+max_clause_length(3).
+
 conc([],L,L).
-conc([X|T],L,[X|L1]):- conc(T,L,L1).
-
-not(A,B,C):-
-    A,
-    B,
-    C, !, fail.
-not(_,_,_).
-
+conc([X|T],L,[X|L1]):-
+    conc(T,L,L1).
 
 ex(predecessor(pam, bob)).
 ex(predecessor(pam, jim)).
@@ -98,30 +89,9 @@ nex(predecessor(pam, liz)).
 nex(predecessor(liz, jim)).
 nex(predecessor(liz, liz)).
 
-ex(has_daughter(pam, ann)).
-ex(has_daughter(tom, liz)).
-ex(has_daughter(bob, pat)).
-
-nex(has_daughter(bob, jim)).
-nex(has_daughter(tom, jim)).
-nex(has_daughter(pam, bob)).
-
 start_hyp([[predecessor(X1,Y1)]/[X1,Y1], [predecessor(X2,Y2)]/[X2,Y2]]).
-start_hyp([[has_daughter(X1,Y1)]/[X1,Y1], [has_daughter(X2,Y2)]/[X2,Y2]]).
 
-prolog_predicate(parent(X, Y)).
-prolog_predicate(predecessor(_,_)).
-prolog_predicate(has_daughter(_,_)).
+backliteral(parent(X,Y), [X,Y]).
+backliteral(predecessor(X,Y), [X,Y]).
 
-learn_predecessor :-
-    write('Learning predecessor...'), nl,
-    induce(HypPredecessor),
-    write('Hypothesis for predecessor: '), write(HypPredecessor), nl.
-
-learn_has_daughter :-
-    write('Learning has_daughter...'), nl,
-    induce(HypHasDaughter),
-    write('Hypothesis for has_daughter: '), write(HypHasDaughter), nl.
-
-:- learn_predecessor.
-:- learn_has_daughter.
+prolog_predicate(parent(_,_)).
